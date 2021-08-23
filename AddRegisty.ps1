4dc06868f659af21a795f825e2492266aae3699d
KAVT35 <kajonsak.vai@gmail.com>
2021-08-23 09:11:18 +0700

    PS AddRegisty

---
+++ AddRegisty.ps1
@@ -0,0 +1,7 @@
+﻿#Initialize the path before add any key or value
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' -Force
+#DisableFeatureUpdateDisplay
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name 'SetDisableUXWUAccess' -PropertyType DWord -Value 1 -Force
+#FixedTargetRelease20H2
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name 'TargetReleaseVersion' -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name 'TargetReleaseVersionInfo' -PropertyType String -Value '20H2' -Force
